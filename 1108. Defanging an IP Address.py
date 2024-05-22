1bc13c025d2c9c4f51fcd3d232b9ef639dc51818
YonSeiLeeJunWon <eximaginary@yonsei.ac.kr>
2024-05-22 15:30:47 +0900

    Delete 1108. Defanging an IP Address.py

--- 1108. Defanging an IP Address.py
+++
@@ -1,6 +0,0 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        return address.replace(".","[.]")
-    
